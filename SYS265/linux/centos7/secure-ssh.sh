48a6a7184762e6fab327dc5320a7d4fdce4fe5f0
Damir Hajrovic <hajrovic.damir@gmail.com>
2021-03-01 10:35:06 -0500

    Update secure-ssh.sh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -1,1 +1,7 @@
 # script file to secure ssh on linux
+useradd -m -d /home/$1 -s /bin/bash $1
+mkdir /home/$1/.ssh
+cp SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+chmod 700 /home/$1/.ssh
+chmod 600 /home/$1/.ssh/authorized_keys
+chown -R $1:$1 /home/$1/.ssh
